fd44911f6dcfffabcb707bf081f06d6e359f0118
ahrbilzakaria <zakariaahr@gmail.com>
2024-03-26 19:41:32 +0000

    rearranging

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { Notfound } from "../pages/Notfound";
-import { About } from "../pages/About";
-import { Topbar } from "../components/Topbar";
-import { Navigation } from "../components/Navigation";
-import { Footer } from "../components/Footer";
+import { Home } from "./pages/Home";
+import { Notfound } from "./pages/Notfound";
+import { About } from "./pages/About";
+import { Topbar } from "./components/Topbar";
+import { Navigation } from "./components/Navigation";
+import { Footer } from "./components/Footer";
 import styles from "./Layout.module.css";
 export const Layout = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Layout } from "./Layout/Layout.jsx";
+import { Layout } from "./Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
